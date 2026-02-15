43bc1ee52f6e4585983ecdf90afb02181a365fcc
ALIN <dev@ALINs-MacBook-Pro.local>
2026-02-15 21:25:46 +0200

    added support for pm2 and config

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps : [
+        {
+            name: "formulaone-web",
+            script: "./start.js",
+            env: {
+                NODE_ENV: "development",
+                PORT: 3000,
+                API_BASE_URL: "https://f1experiences.co.uk/api/public",
+            },
+            env_production: {
+                NODE_ENV: "production",
+                PORT: 3000,
+                API_BASE_URL: "https://f1experiences.co.uk/api/public",
+            },
+            watch: true,
+            ignore_watch: ["node_modules", "dist", "build", "public", "storage", "vendor", "logs"],
+            max_memory_restart: "1G",
+        }
+    ]
+}
---
+++ start.js
@@ -0,0 +1,14 @@
+// start.js
+const { exec } = require('child_process');
+
+exec('next start -p 3000', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Stdout: ${stdout}`);
+});
